717acfc0bfda7fb7c129c274f3a379d398270a42
Juanma <juanmaninc@gmail.com>
2023-02-12 23:28:24 -0300

    Se corrigio el title de og

--- src/app/head.jsx
+++ src/app/head.jsx
@@ -9,7 +9,7 @@ export default function Head() {
       
       <meta property="og:url" content={process.env.NEXT_PUBLIC__PUBLIC_BASE_DNS} />            
 
-      <meta property="og:title" content="JM portafio" />
+      <meta property="og:title" content="JM portaflio" />
       <meta property="og:image" content={`${process.env.NEXT_PUBLIC__PUBLIC_BASE_DNS}/img/cover.jpg`} />
       <meta property="og:description" content="Desarrollador web frontend - JM" />
       <meta name="twitter:title" content="JM portafolio" />
