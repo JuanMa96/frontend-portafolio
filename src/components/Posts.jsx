ccfa2dc16afe4b9cb9569b065769a65429336a14
Juanma <juanmaninc@gmail.com>
2023-02-18 21:14:19 -0300

    Corregí el espacio raro en los posts

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -13,7 +13,7 @@ import Link from 'next/link';
         return (
         <>    
             {docs.map((post)=>(
-                <article key={post.id} className=" bg-white border-slate-800 dark:bg-slate-800 dark:border-white border-4 rounded-xl shadow-xl">
+                <article key={post.id} className=" bg-white border-slate-800 dark:bg-slate-800 dark:border-white border-4 rounded-xl shadow-xl flex flex-col items-center justify-between gap-3">
                     <Link href={`/posts/${post.id}`} as={`/posts/${post.id}`}>
                         <Image src={post.image.url}
                             width={post.image.width}
@@ -24,11 +24,11 @@ import Link from 'next/link';
                                33vw"
                             />        
                     </Link>
-                    <div className='p-3 flex flex-col items-center gap-3'>    
-                        <h2 className='text-slate-900 dark:text-white text-2xl'>{post.title}</h2>
-                        <p className='text-slate-500 dark:text-slate-400'>
-                            {post.description}
-                        </p>
+                    <h2 className='text-slate-900 dark:text-white text-2xl px-3'>{post.title}</h2>
+                    <p className='text-slate-500 dark:text-slate-400 px-3'>
+                        {post.description}
+                    </p>
+                    <div className='pb-3'>
                         <Link href={`/posts/${post.id}`} as={`/posts/${post.id}`} className="py-1 px-3 text-center bg-yellow-400 text-black dark:bg-white dark:text-black rounded-xl shadow-sm shadow-slate-200 dark:shadow-white">
                             Ver más
                         </Link>
